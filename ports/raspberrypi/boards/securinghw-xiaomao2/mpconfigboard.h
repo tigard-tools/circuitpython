5e4c5612d5519bded5d550e20d8ff9385e9f1912
@securelyfitz <joefitz@joefitz.net>
2026-04-14 23:07:24 -0700

    inital pass at adding xiaomao2 support

---
+++ ports/raspberrypi/boards/securinghw-xiaomao2/mpconfigboard.h
@@ -0,0 +1,17 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2021 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#define MICROPY_HW_BOARD_NAME "Xiaomao 2"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO7)
+#define CIRCUITPY_STATUS_LED_POWER (&pin_GPIO6)
+
+#define CIRCUITPY_BOARD_UART        (2)
+#define CIRCUITPY_BOARD_UART_PIN    {{.tx = &pin_GPIO16, .rx = &pin_GPIO17}, \
+                                     {.tx = &pin_GPIO20, .rx = &pin_GPIO21}}
